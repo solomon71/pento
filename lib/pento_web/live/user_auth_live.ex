ff12f9dc73a615dcf1cea7f2967d42fd5162a5cb
Paul Solomon <paul@thinkingbox.com>
2022-08-05 21:55:31 -0600

    adding products table

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -4,15 +4,16 @@ defmodule PentoWeb.Live.UserAuthLive do
   alias Pento.Accounts
   alias PentoWeb.Router.Helpers, as: Routes
 
-  def on_mount(_, params, %{"user_token" => user_token}, socket) do
-    user = Accounts.get_user_by_session_token(user_token)
-
+  def on_mount(_, _params, %{"user_token" => user_token}, socket) do
+    # user = Accounts.get_user_by_session_token(user_token)
+    # IO.inspect(user_token)
     # IO.inspect(user)
     # IO.inspect(params)
 
     socket =
       socket
-      |> assign(:current_user, user)
+      |> assign_new(:current_user, fn -> Accounts.get_user_by_session_token(user_token) end)
+      # |> assign(:current_user, user)
 
     if socket.assigns.current_user do
       {:cont, socket}
--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -77,6 +77,13 @@ defmodule PentoWeb.Router do
     pipe_through [:browser, :require_authenticated_user]
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
 
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
+
     live_session :default, on_mount: PentoWeb.Live.UserAuthLive do
       get "/users/settings", UserSettingsController, :edit
       put "/users/settings", UserSettingsController, :update
